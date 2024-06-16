f13500c989db414776d1d919d8bc6623686c1b75
Samwel <samwelnyandoro01@gmail.com>
2024-06-16 12:28:11 +0300

    first commit

---
+++ app/head.tsx
@@ -0,0 +1,24 @@
+export default function Head() {
+  return (
+    <>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="I am an Android developer based in Nairobi, Kenya. I'm a Business Information Technology graduate from KHU, and my journey in App development began during my first year of college. I'm passionate about creating beautiful, functional, and user-friendly applications, and I'm constantly pushing myself to learn and grow as a developer. Love building full-stack applications and side projects." />
+      <meta name="keywords" content="Samwel Nyandoro, portfolio, jigar, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio" />
+      <meta property="og:site_name" content="Samwel Nyandoro's Portfolio" />
+      <meta property="og:title" content="Portfolio | Samwel Nyandoro - Full Stack Developer" />
+      <meta property="og:description"
+        content="I am an Android developer based in Nairobi, Kenya. I'm a Business Information Technology graduate from KHU, and my journey in App development began during my first year of college. I'm passionate about creating beautiful, functional, and user-friendly applications, and I'm constantly pushing myself to learn and grow as a developer. Love building full-stack applications and side projects." />
+      <meta property="og:url" content="https://samwelnyandoro.vercel.app" />
+      <meta property="og:image" content="https://samwelnyandoro.vercel.app/portfolio-fork.png" />
+      <link rel="canonical" href="https://samwelnyandoro.vercel.app" />
+      <meta name="author" content="Samwel Nyandoro" />
+      <meta name="language" content="English" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="https://samwelnyandoro.vercel.app" />
+      <meta name="application-name" content="Portfolio | Samwel Nyandoro - Full Stack Developer" />
+      <meta name="apple-mobile-web-app-title" content="Portfolio | Samwel Nyandoro - Full Stack Developer" />
+      <link rel="icon" href="/favicon.ico" />
+      <title>Portfolio | Samwel Nyandoro - Android Developer</title>
+    </>
+  )
+}
